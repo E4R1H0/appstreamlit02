90df8a535929088c137e6b94975436465d56d5e9
EEEE <119985774+E4R1H0@users.noreply.github.com>
2023-09-26 15:00:29 +0700

    .commit

--- Mystreamlit.py
+++ Mystreamlit.py
@@ -1,7 +1,7 @@
 import streamlit as st
 
-st.image('/.pic/apakaearth.jpg')
-st.image('/.pic/iris01.jpg')
-st.image('/.pic/iris02.jpg')
-st.image('/.pic/iris03.jpg')
+st.image('./pic/apakaearth.jpg')
+st.image('./pic/iris01.jpg')
+st.image('./pic/iris02.jpg')
+st.image('./pic/iris0.jpg')
 st.header('Nonthakan jarpun') 
