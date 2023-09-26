05e57020934411d3e566a1799ce148b1fe12b5f1
EEEE <119985774+E4R1H0@users.noreply.github.com>
2023-09-26 15:07:25 +0700

    .commit

--- Mystreamlit.py
+++ Mystreamlit.py
@@ -1,12 +1,8 @@
 import streamlit as st
+st.image('./pic/apakaearth.jpg')
 col1,col2=st.colums(2)
 with col1 :
     st.header('Nonthakan jarpun')
 with col2 :
     st.subheader('สาขาวิทยาการข้อมูล')
     st.text('คณะวิทยาศาสตร์และเทคโนโลยี')
-st.image('./pic/apakaearth.jpg')
-st.image('./pic/iris01.jpg')
-st.image('./pic/iris02.jpg')
-st.image('./pic/iris03.jpg')
-st.header('Nonthakan jarpun') 
