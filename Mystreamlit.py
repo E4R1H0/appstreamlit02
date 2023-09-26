72ba718676aad3c389ba0bcb017e732c7d86e5c8
EEEE <119985774+E4R1H0@users.noreply.github.com>
2023-09-26 15:05:17 +0700

    .commit

--- Mystreamlit.py
+++ Mystreamlit.py
@@ -1,5 +1,10 @@
 import streamlit as st
-
+col1,col2=st.colums(2)
+with col1 :
+    st.header('Nonthakan jarpun')
+with col2 :
+    st.subheader('สาขาวิทยาการข้อมูล')
+    st.text('คณะวิทยาศาสตร์และเทคโนโลยี')
 st.image('./pic/apakaearth.jpg')
 st.image('./pic/iris01.jpg')
 st.image('./pic/iris02.jpg')
