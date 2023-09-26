b3c2fa648ec30b50a7b6d3932dec66ddde63e6b8
EEEE <119985774+E4R1H0@users.noreply.github.com>
2023-09-26 15:10:13 +0700

    .commit

--- Mystreamlit.py
+++ Mystreamlit.py
@@ -7,3 +7,11 @@ with col1 :
 with col2 :
     st.subheader('สาขาวิทยาการข้อมูล')
     st.text('คณะวิทยาศาสตร์และเทคโนโลยี')
+
+html_1 = """
+<div style="background-color:#52BE80;padding:15px;border-radius:15px 15px 15px 15px;border-style:'solid';border-color:black">
+<center><h5>สถิติข้อมูลดอกไม้</h5></center>
+</div>
+"""
+st.markdown(html_1, unsafe_allow_html=True)
+st.markdown("")
